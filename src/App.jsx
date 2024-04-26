65b106a963462a8b1a17e227d0ff4f0adfb331e4
Emanuele-P <161461547+Emanuele-P@users.noreply.github.com>
2024-04-26 14:58:05 +0200

    added extra

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,18 @@
-import './App.css';
-import AllTheBooks from './components/AllTheBooks';
-import MyAlert from './components/MyAlert';
-import MyFooter from './components/MyFooter';
-import MyNav from './components/MyNav';
+import './App.scss'
+import React, { useState } from 'react'
+import AllTheBooks from './components/AllTheBooks'
+import MyAlert from './components/MyAlert'
+import MyFooter from './components/MyFooter'
+import MyNav from './components/MyNav'
 import fantasy from './data/books/fantasy.json'
 import romance from './data/books/romance.json'
 import history from './data/books/history.json'
 import scifi from './data/books/scifi.json'
 import horror from './data/books/horror.json'
-import SingleBook from './components/SingleBook';
-import BookList from './components/BookList';
+import BookList from './components/BookList'
 
 function App() {
-  // const book = {
-  //   title: 'Sword of Destiny (The Witcher)',
-  //   img: 'https://images-na.ssl-images-amazon.com/images/I/91uxJwnolDL.jpg',
-  // }
+  const [booksData, setBooksData] = useState(fantasy)
 
   return (
     <div className="App">
@@ -24,23 +21,37 @@ function App() {
       </header>
 
       <main>
-      <MyAlert></MyAlert>
+        <MyAlert></MyAlert>
 
-      <section className='hero'>
-      <BookList booksData={fantasy} />
-      </section>
+        <section className="hero">
+          <BookList booksData={booksData} setBooksData={setBooksData} />
+        </section>
 
-      <AllTheBooks headerText={'Fantasy Books'} booksData={fantasy}></AllTheBooks>
-      <AllTheBooks headerText={'Romance Books'} booksData={romance}></AllTheBooks>
-      <AllTheBooks headerText={'History Books'} booksData={history}></AllTheBooks>
-      <AllTheBooks headerText={'Sci-fi Books'} booksData={scifi}></AllTheBooks>
-      <AllTheBooks headerText={'Horror Books'} booksData={horror}></AllTheBooks>
-
+        <AllTheBooks
+          headerText={'Fantasy Books'}
+          booksData={fantasy}
+        ></AllTheBooks>
+        <AllTheBooks
+          headerText={'Romance Books'}
+          booksData={romance}
+        ></AllTheBooks>
+        <AllTheBooks
+          headerText={'History Books'}
+          booksData={history}
+        ></AllTheBooks>
+        <AllTheBooks
+          headerText={'Sci-fi Books'}
+          booksData={scifi}
+        ></AllTheBooks>
+        <AllTheBooks
+          headerText={'Horror Books'}
+          booksData={horror}
+        ></AllTheBooks>
       </main>
 
       <MyFooter></MyFooter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,42 +1,86 @@
-import { useState } from "react";
-import { Col, Form, Row } from "react-bootstrap";
-import SingleBook from "./SingleBook";
+import React, { Component } from 'react'
+import { Col, Container, Form, Row } from 'react-bootstrap'
+import SingleBook from './SingleBook'
+import ToggleGroup from './ToggleGroup'
+import fantasy from '../data/books/fantasy.json'
+import romance from '../data/books/romance.json'
+import history from '../data/books/history.json'
+import scifi from '../data/books/scifi.json'
+import horror from '../data/books/horror.json'
 
-function BookList({ booksData }) {
+class BookList extends Component {
+  state = {
+    searchTerm: '',
+    booksData: [...fantasy, ...romance, ...history, ...scifi, ...horror],
+    selectedBookAsin: null,
+  }
 
-    const [searchTerm, setSearchTerm] = useState('')
+  scrollRef = React.createRef()
 
-    const handleSearchChange = (e) => {
-        setSearchTerm(e.target.value)
+  handleSearchChange = (e) => {
+    this.setState({ searchTerm: e.target.value })
+  }
+
+  handleBookSelect = (asin) => {
+    this.setState({ selectedBookAsin: asin })
+  }
+
+  setBooksData = (data) => {
+    this.setState({ booksData: data })
+  }
+
+  handleGenreChange = (genre) => {
+    const genreMap = {
+      1: [...fantasy, ...romance, ...history, ...scifi, ...horror],
+      2: fantasy,
+      3: romance,
+      4: history,
+      5: scifi,
+      6: horror,
     }
+    this.setState({ booksData: genreMap[genre] || [] })
+  }
 
-    const filteredBooks = booksData.filter(book => 
-    book.title.toLowerCase().includes(searchTerm.toLowerCase()))
-
+  render() {
+    const { searchTerm, booksData, selectedBookAsin } = this.state
+    const filteredBooks = booksData.filter((book) =>
+      book.title.toLowerCase().includes(searchTerm.toLowerCase())
+    )
     return (
-
-        <div>
-        <Form>
-          <Form.Group as={Row} controlId="searchTerm">
-            <Form.Label column sm={2}>Search:</Form.Label>
-            <Col lg={4}>
-              <Form.Control
-                type="text"
-                placeholder="Search by title"
-                value={searchTerm}
-                onChange={handleSearchChange}
-              />
-            </Col>
-          </Form.Group>
-        </Form>
-        <div className="d-flex flex-wrap">
-          {filteredBooks.map(book => (
-            <SingleBook key={book.asin} book={book} />
+      <Container>
+        <Row className="search-row mt-5 mb-2">
+          <Col xs={12} md={8} lg={6} xl={4} className="p-0">
+            <ToggleGroup onGenreChange={this.handleGenreChange} />
+          </Col>
+          <Col xs={12} md={4} lg={6} xl={8} className="mt-3 mt-md-0 p-0">
+            <Form>
+              <Form.Group controlId="searchTerm">
+                <div className="search-input-container">
+                  <Form.Control
+                    className="custom-form-control"
+                    type="text"
+                    placeholder="Search books by title"
+                    value={searchTerm}
+                    onChange={this.handleSearchChange}
+                  />
+                </div>
+              </Form.Group>
+            </Form>
+          </Col>
+        </Row>
+        <div className="horizontal-scroll d-flex" ref={this.scrollRef}>
+          {filteredBooks.map((book, i) => (
+            <SingleBook
+              key={`${book.asin}-${i}`}
+              book={book}
+              onSelect={this.handleBookSelect}
+              isSelected={book.asin === selectedBookAsin}
+            />
           ))}
         </div>
-      </div>
-    );
-
+      </Container>
+    )
+  }
 }
 
 export default BookList
